806c3fe47a86d887736abb06aaac121374a67463
Amelia <53657436+AmeliaTYR@users.noreply.github.com>
2023-05-13 18:43:34 +0800

    Add basic scripts, issue with mlagents

--- ChopsticksUnity/Assets/Scripts/PlayerAgent.cs
+++ ChopsticksUnity/Assets/Scripts/PlayerAgent.cs
@@ -1,18 +1,481 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
+using UnityEngine.UI;
+using TMPro;
+
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 using UnityEngine;
 
-public class PlayerAgent : MonoBehaviour
+/// <summary>
+/// A player agent for chopsticks
+/// </summary>
+public class PlayerAgent : Agent
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("Player number")]
+    public int playerNum = 1;
+
+    [Tooltip("Num fingers left hand")]
+    public int leftNum = 1;
+
+    [Tooltip("Num fingers right hand")]
+    public int rightNum = 1;
+
+    [Tooltip("Label for the left hand")]
+    public TextMeshProUGUI leftLabel;
+
+    [Tooltip("Label for the right hand")]
+    public TextMeshProUGUI rightLabel;
+
+    // Refernce to the other player, shown in the inspector
+    [SerializeField]
+    private PlayerAgent otherPlayer;
+
+    [SerializeField]
+    private Referee referee;
+
+    private bool foundOtherObjects = false;
+
+    /// <summary>
+    /// Initialize the agent
+    /// </summary>
+    public override void Initialize()
     {
-        
+        // base.Initialize();
+
+        // Get the child canvas
+        Transform canvas = transform.Find("Canvas");
+        if (canvas == null)
+        {
+            Debug.LogError("Could not find canvas");
+        }
+        else
+        {
+            // Get the child text objects
+            leftLabel = canvas.Find("LeftHandNum").GetComponent<TextMeshProUGUI>();
+            rightLabel = canvas.Find("RightHandNum").GetComponent<TextMeshProUGUI>();
+
+        }
+
+        // Set the initial values 
+        leftNum = 1;
+        rightNum = 1;
+
+        if (leftLabel == null || rightLabel == null)
+        {
+            Debug.LogError("Could not find left or right label");
+        }
+        else
+        {
+            // Set the labels to the initial values
+            leftLabel.text = leftNum.ToString();
+            rightLabel.text = rightNum.ToString();
+        }
+
+        // Get the other player from within the parent game object, not itself
+        PlayerAgent[] playersInGameArena = transform.parent.GetComponentsInChildren<PlayerAgent>();
+        foreach (PlayerAgent player in playersInGameArena)
+        {
+            if (player != this)
+            {
+                otherPlayer = player;
+                break;
+            }
+        }
+
+        if (otherPlayer == null)
+        {
+            Debug.LogError("Could not find other player");
+        }
+
+        // Get the referee
+        referee = transform.parent.GetComponent<Referee>();
+        if (referee == null)
+        {
+            Debug.LogError("Could not find referee");
+        }
+
+        // TODO: Remove this later
+        // Test getting the other player's numbers
+        Debug.Log("Other player left num: " + otherPlayer.GetLeftNum());
+        Debug.Log("Other player right num: " + otherPlayer.GetRightNum());
+
+        if (otherPlayer != null && leftLabel != null && rightLabel != null && referee != null)
+        {
+            foundOtherObjects = true;
+        }
+        else
+        {
+            Debug.LogError("Could not find other player, left label, right label, or referee");
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// An agent is reset when an episode begins
+    /// </summary>
+    public override void OnEpisodeBegin()
     {
-        
+        // base.OnEpisodeBegin();
+
+        // Reset the left and right hand numbers to 1
+        leftNum = 1;
+        rightNum = 1;
     }
+
+    /// <summary>
+    /// Called when an action is recieved from either the player or the neural network
+    /// </summary>
+    /// <param name="actions">The actions to take</param>
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        // base.OnActionReceived(actions);
+
+        // ActionSegment<float> vectorAction = actions.ContinuousActions;
+        ActionSegment<int> vectorAction = actions.DiscreteActions;
+
+        // Possible actions: attack (4 options), split (6 options, some not feasible) or self-add (2 options)
+        // Attack: 0, 1, 2, 3
+        // Split: 4, 5, 6, 7, 8, 9
+        // Self-add: 10, 11
+
+        // Attack
+        if (vectorAction[0] < 4)
+        {
+            // Attack the other player's hand
+            // 0: 1L - 2L
+            // 1: 1L - 2R
+            // 2: 1R - 2L
+            // 3: 1R = 2R
+
+            if (vectorAction[0] == 0)
+            {
+                referee.PlayerResponse(leftNum, rightNum, otherPlayer.GetLeftNum() + leftNum, otherPlayer.GetRightNum(), playerNum);
+            }
+            else if (vectorAction[0] == 1)
+            {
+                referee.PlayerResponse(leftNum, rightNum, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum() + leftNum, playerNum);
+            }
+            else if (vectorAction[0] == 2)
+            {
+                referee.PlayerResponse(rightNum, leftNum, otherPlayer.GetLeftNum() + rightNum, otherPlayer.GetRightNum(), playerNum);
+            }
+            else if (vectorAction[0] == 3)
+            {
+                referee.PlayerResponse(rightNum, leftNum, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum() + rightNum, playerNum);
+            }
+
+        }
+
+        // Split
+        else if (vectorAction[0] < 10)
+        {
+            // Split the hand
+            // 4-6: move 1-3 from L-R (3*1 = 3 moves)
+            // 7-9: move 1-3 from R-L (3*1 = 3 moves)
+
+            if (vectorAction[0] == 4)
+            {
+                referee.PlayerResponse(leftNum - 1, rightNum + 1, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum(), playerNum);
+            }
+            else if (vectorAction[0] == 5)
+            {
+                referee.PlayerResponse(leftNum - 2, rightNum + 2, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum(), playerNum);
+            }
+            else if (vectorAction[0] == 6)
+            {
+                referee.PlayerResponse(leftNum - 3, rightNum + 3, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum(), playerNum);
+            }
+            else if (vectorAction[0] == 7)
+            {
+                referee.PlayerResponse(leftNum + 1, rightNum - 1, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum(), playerNum);
+            }
+            else if (vectorAction[0] == 8)
+            {
+                referee.PlayerResponse(leftNum + 2, rightNum - 2, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum(), playerNum);
+            }
+            else if (vectorAction[0] == 9)
+            {
+                referee.PlayerResponse(leftNum + 3, rightNum - 3, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum(), playerNum);
+            }
+        }
+
+        // Self-add
+        else
+        {
+            // Add to the hand
+            // 10: 1L - 1R
+            // 11: 1R - 1L
+
+            if (vectorAction[0] == 10)
+            {
+                referee.PlayerResponse(leftNum, rightNum + leftNum, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum(), playerNum);
+            }
+            else if (vectorAction[0] == 11)
+            {
+                referee.PlayerResponse(leftNum + rightNum, rightNum, otherPlayer.GetLeftNum(), otherPlayer.GetRightNum(), playerNum);
+            }
+
+        }
+
+    }
+
+    /// <summary>
+    /// Collect vector observations from the environment
+    /// </summary>
+    /// <param name="sensor">The vector sensor</param>
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // base.CollectObservations(sensor);
+
+        if (!foundOtherObjects)
+        {
+            Debug.LogError("Could not find other objects");
+            sensor.AddObservation(new float[4]);
+            return;
+        }
+
+
+        // Add the left and right hand numbers to the observations
+        sensor.AddObservation(leftNum / 4);
+        sensor.AddObservation(rightNum / 4);
+
+        // Add the other player's left and right hand numbers to the observations
+        sensor.AddObservation(otherPlayer.GetLeftNum() / 4);
+        sensor.AddObservation(otherPlayer.GetRightNum() / 4);
+
+        // 4 total observations
+    }
+
+    // Masking the actions
+    // https://unity-technologies.github.io/ml-agents/Learning-Environment-Design-Agents/#decisions:~:text=300f%2C%20directionZ%20*%2040f))%3B-,Masking%20Discrete%20Actions,-When%20using%20Discrete
+
+    /// <summary>
+    /// Mask illegal actions
+    /// </summary>
+    /// <param name="actionMask">The action mask</param>
+    public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask)
+    {
+        // base.WriteDiscreteActionMask(actionMask);
+
+        // Format: actionMask.SetActionEnabled(branch, actionIndex, isEnabled);
+
+        // set all actions to enabled first
+        for (int i = 0; i < 12; i++)
+        {
+            // NOTE: if cannot, replace with temp array
+            actionMask.SetActionEnabled(0, i, true);
+        }
+
+        // Get the other player's numbers
+        int otherLeftNum = otherPlayer.GetLeftNum();
+        int otherRightNum = otherPlayer.GetRightNum();
+
+        // Disable actions that are not possible
+        if (leftNum == 0)
+        {
+            // Can't attack
+            actionMask.SetActionEnabled(0, 0, false);
+            actionMask.SetActionEnabled(0, 1, false);
+
+            // Can't split L-R
+            actionMask.SetActionEnabled(0, 4, false);
+            actionMask.SetActionEnabled(0, 5, false);
+            actionMask.SetActionEnabled(0, 6, false);
+
+            // Can't self-add 1L - 1R
+            actionMask.SetActionEnabled(0, 10, false);
+        }
+        else
+        {
+            if (otherPlayer.GetLeftNum() == 0)
+            {
+                // Can't attack left hand
+                actionMask.SetActionEnabled(0, 0, false);
+            }
+            if (otherPlayer.GetRightNum() == 0)
+            {
+                // Can't attack right hand
+                actionMask.SetActionEnabled(0, 1, false);
+            }
+        }
+
+        if (rightNum == 0)
+        {
+            // Can't attack
+            actionMask.SetActionEnabled(0, 2, false);
+            actionMask.SetActionEnabled(0, 3, false);
+
+            // Can't split R-L
+            actionMask.SetActionEnabled(0, 7, false);
+            actionMask.SetActionEnabled(0, 8, false);
+            actionMask.SetActionEnabled(0, 9, false);
+
+            // Can't self-add 1R - 1L
+            actionMask.SetActionEnabled(0, 11, false);
+        }
+        else
+        {
+            if (otherPlayer.GetLeftNum() == 0)
+            {
+                // Can't attack left hand
+                actionMask.SetActionEnabled(0, 2, false);
+            }
+            if (otherPlayer.GetRightNum() == 0)
+            {
+                // Can't attack right hand
+                actionMask.SetActionEnabled(0, 3, false);
+            }
+        }
+
+        // If the result of a split is flipped L-R values, disable it
+        if (leftNum - 1 == rightNum && rightNum + 1 == leftNum)
+        {
+            // Can't split L-R 1
+            actionMask.SetActionEnabled(0, 4, false);
+        }
+        if (leftNum - 2 == rightNum && rightNum + 2 == leftNum)
+        {
+            // Can't split L-R 2
+            actionMask.SetActionEnabled(0, 5, false);
+        }
+        if (leftNum - 3 == rightNum && rightNum + 3 == leftNum)
+        {
+            // Can't split L-R 3
+            actionMask.SetActionEnabled(0, 6, false);
+        }
+
+        // If the result of a split is flipped R-L values, disable it
+        if (rightNum - 1 == leftNum && leftNum + 1 == rightNum)
+        {
+            // Can't split R-L 1
+            actionMask.SetActionEnabled(0, 7, false);
+        }
+        if (rightNum - 2 == leftNum && leftNum + 2 == rightNum)
+        {
+            // Can't split R-L 2
+            actionMask.SetActionEnabled(0, 8, false);
+        }
+        if (rightNum - 3 == leftNum && leftNum + 3 == rightNum)
+        {
+            // Can't split R-L 3
+            actionMask.SetActionEnabled(0, 9, false);
+        }
+
+
+    }
+
+
+    /// <summary>
+    /// When behaviour type is set to "Heuristic Only" on the agent's Behaviour Parameters,
+    /// this function will be called. Its return values will be fed into
+    /// <see cref="Heuristic(in ActionBuffers)"/> instead of using the neural network 
+    /// </summary>
+    /// <param name = "actionsOut">An output action buffer</param>
+    public override void Heuristic(in ActionBuffers actionsOutBuffer)
+    {
+        // base.Heuristic(actionsOut);
+
+        // Get the action buffer
+        ActionSegment<int> actionsOut = actionsOutBuffer.DiscreteActions;
+
+        // user press enter first
+        while (true)
+        {
+            if (Input.GetKey(KeyCode.Return))
+            {
+                break;
+            }
+        }
+
+        // Wait for input in a while loop
+        while (true)
+        {
+            if (Input.GetKey(KeyCode.Alpha0))
+            {
+                actionsOut[0] = 0;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Alpha1))
+            {
+                actionsOut[0] = 1;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Alpha2))
+            {
+                actionsOut[0] = 2;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Alpha3))
+            {
+                actionsOut[0] = 3;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Alpha4))
+            {
+                actionsOut[0] = 4;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Alpha5))
+            {
+                actionsOut[0] = 5;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Alpha6))
+            {
+                actionsOut[0] = 6;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Alpha7))
+            {
+                actionsOut[0] = 7;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Alpha8))
+            {
+                actionsOut[0] = 8;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Alpha9))
+            {
+                actionsOut[0] = 9;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Plus))
+            {
+                actionsOut[0] = 10;
+                break;
+            }
+            else if (Input.GetKey(KeyCode.Minus))
+            {
+                actionsOut[0] = 11;
+                break;
+            }
+        }
+
+
+
+    }
+
+    /// <summary>
+    /// Function which returns the Agent current left hand num
+    /// </summary>
+    /// <returns>Left hand num</returns>
+    public int GetLeftNum()
+    {
+        return leftNum;
+    }
+
+    /// <summary>
+    /// Function which returns the Agent current right hand num
+    /// </summary>
+    /// <returns>Right hand num</returns>
+    public int GetRightNum()
+    {
+        return rightNum;
+    }
+
 }
--- ChopsticksUnity/Assets/Scripts/Referee.cs
+++ ChopsticksUnity/Assets/Scripts/Referee.cs
@@ -4,15 +4,201 @@ using UnityEngine;
 
 public class Referee : MonoBehaviour
 {
+    // Current player turn
+    [SerializeField]
+    public int currentPlayerTurn = 1;
+
+    [SerializeField]
+    public string gameState = "idle";
+
+    // Reference to the players
+    public PlayerAgent player1;
+    public PlayerAgent player2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Find player 1 and 2
+        PlayerAgent[] playersInGameArena = null;
+
+        playersInGameArena = transform.GetComponentsInChildren<PlayerAgent>();
+
+        // if len not 2 search again
+        if (playersInGameArena.Length != 2)
+        {
+            playersInGameArena = null;
+        }
+
+        foreach (PlayerAgent player in playersInGameArena)
+        {
+            if (player.playerNum == 1)
+            {
+                player1 = player;
+            }
+            else if (player.playerNum == 2)
+            {
+                player2 = player;
+            }
+        }
+
+
+        if (player1 == null || player2 == null)
+        {
+            playersInGameArena = null;
+        }
+        else
+        {
+            Debug.Log("Players found");
+        }
+
+
+
+        // Set the game state
+        gameState = "playing";
+
+        // Call player 1
+        CallPlayer1();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CallPlayer1()
     {
-        
+        player1.RequestDecision();
     }
+
+    private void CallPlayer2()
+    {
+        player2.RequestDecision();
+    }
+
+    public void PlayerResponse(int p1Left, int p1Right, int p2Left, int p2Right, int playerNum)
+    {
+        if (playerNum == 1)
+        {
+            Player1TurnResponse(p1Left, p1Right, p2Left, p2Right);
+        }
+        else if (playerNum == 2)
+        {
+            Player2TurnResponse(p2Left, p2Right, p1Left, p1Right);
+        }
+    }
+
+    // Player 1 returns a new game state as an action
+    public void Player1TurnResponse(int p1Left, int p1Right, int p2Left, int p2Right)
+    {
+        // Update the game state
+        if (IsGameOver(p1Left, p1Right, p2Left, p2Right))
+        {
+            Debug.Log("Game over");
+            return;
+        }
+
+        // Check if a hand was killed
+        if (p1Left == 0)
+        {
+            player1.AddReward(-0.01f);
+        }
+        if (p1Right == 0)
+        {
+            player1.AddReward(-0.01f);
+        }
+
+        // Check if a opponent hand was killed
+        if (p2Left == 0)
+        {
+            player1.AddReward(0.03f);
+        }
+        if (p2Right == 0)
+        {
+            player1.AddReward(0.03f);
+        }
+
+
+        // Update the game state for player 1
+        player1.leftNum = p1Left;
+        player1.rightNum = p1Right;
+
+        // Update the game state for player 2
+        player2.leftNum = p2Left;
+        player2.rightNum = p2Right;
+
+        // Change player turn
+        currentPlayerTurn = 2;
+
+        // Call player 2
+        CallPlayer2();
+
+    }
+
+    // Player 2 returns a new game state as an action
+    public void Player2TurnResponse(int p2Left, int p2Right, int p1Left, int p1Right)
+    {
+        // Update the game state
+        if (IsGameOver(p2Left, p2Right, p1Left, p1Right))
+        {
+            Debug.Log("Game over");
+            return;
+        }
+
+        // Check if a hand was killed
+        if (p2Left == 0)
+        {
+            player2.AddReward(-0.01f);
+        }
+        if (p2Right == 0)
+        {
+            player2.AddReward(-0.01f);
+        }
+
+        // Check if a opponent hand was killed
+        if (p1Left == 0)
+        {
+            player2.AddReward(0.03f);
+        }
+        if (p1Right == 0)
+        {
+            player2.AddReward(0.03f);
+        }
+
+        // Update the game state for player 1
+        player1.leftNum = p1Left;
+        player1.rightNum = p1Right;
+
+        // Update the game state for player 2
+        player2.leftNum = p2Left;
+        player2.rightNum = p2Right;
+
+        // Change player turn
+        currentPlayerTurn = 1;
+
+        // Call player 2
+        CallPlayer2();
+
+    }
+
+    public bool IsGameOver(int p1Left, int p1Right, int p2Left, int p2Right)
+    {
+        if (p1Left == 0 && p1Right == 0)
+        {
+            gameState = "player2Win";
+            player1.AddReward(-1f);
+            player2.AddReward(1f);
+        }
+        else if (p2Left == 0 && p2Right == 0)
+        {
+            gameState = "player1Win";
+            player2.AddReward(-1f);
+            player1.AddReward(1f);
+        }
+
+        if (gameState == "player1Win" || gameState == "player2Win")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
